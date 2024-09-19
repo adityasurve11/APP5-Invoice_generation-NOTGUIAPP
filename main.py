05560c2be7b7ac4202bbc1a77aac4b633a5d928c
ADITYA <aditya11surve08@gmail.com>
2024-09-19 14:17:30 +0530

    Day 26: Duplicated code

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
     invoice_nr = filename.split("_")[0]
+
     pdf.set_font(family="Times", size=18, style="B")
-    pdf.cell(w=50, h=8, txt="Invoice nr.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+
+    pdf.set_font(family="Times", size=18, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {invoice_nr}")
+
+
     pdf.output("PDFs/{filename}.pdf")
